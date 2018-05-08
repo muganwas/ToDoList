84fab36ffd6813f2c0cb8b92a1c5a3de24a0a211
Muganwa <stevenmuganwa@live.com>
2018-05-08 21:27:41 +0300

    Initial Push

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 4000,
+mongoose = require('mongoose'),
+Task = require('./api/models/todoListModel'),
+bodyParser = require('body-parser');
+//mongo db connection string
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res){
+    res.status(404).send({url: req.originalUrl + " Not Found"})
+})
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log("todoList RESTful API server started at " + port);
