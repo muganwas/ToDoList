4b19e35ea2113e1322925590c636f941dd6f2c00
Muganwa <stevenmuganwa@live.com>
2018-05-09 11:15:55 +0300

    comments

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ Task = require('./api/models/todoListModel'),
 bodyParser = require('body-parser');
 //mongo db connection string
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://alien:dynamics@ds117540.mlab.com:17540/aliendev', function(err){
+mongoose.connect('mongodb://localhost/Tododb', function(err){
     if(err)
         throw err;
-    console.log("successful connection");
+    console.log("connected successfully");
 });
 
-
 app.use(bodyParser.urlencoded({extended: true}));
+/*automatically sends this
+**************************
 app.use(function(req, res){
     res.status(404).send({url: req.originalUrl + " Not Found"})
-})
+});
+*/
 app.use(bodyParser.json());
 
 var routes = require('./api/routes/todoListRoutes');
