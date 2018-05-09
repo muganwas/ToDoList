7abb586179dc714e26ab825a9830b5c0df1c2d66
Muganwa <stevenmuganwa@live.com>
2018-05-09 16:40:29 +0300

    test3-

--- api/controllers/notificationController.js
+++ api/controllers/notificationController.js
@@ -6,17 +6,19 @@ DoWebPaymentResult = mongoose.model('DoWebPaymentResult');
 
 //getwebpament details
 function getWebPaymentDetails(token){
+    /*
     jquery.post('https://dev.aliendynamic.com/examples/web/getWebPaymentDetails.php',
      { 
        token: token
     },function(success){
         console.log(success);
     });
+    */
 }
 exports.storeTransactionInfo = function (req, res){
     var notificationType = req.query.notificationType;
     var token = req.query.token;
-    getWebPaymentDetails(token);
+    //getWebPaymentDetails(token);
     var obj = {notificationType: notificationType, token:token}
     var newDoWebPaymentResult = new DoWebPaymentResult(obj);
     newDoWebPaymentResult.save(function(err, detail){
