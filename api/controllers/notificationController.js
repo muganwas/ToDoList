dd04cf2299c0c5271218685db6193be17ff1cd88
Muganwa <stevenmuganwa@live.com>
2018-05-09 21:46:57 +0300

    send mail params test

--- api/controllers/notificationController.js
+++ api/controllers/notificationController.js
@@ -2,19 +2,7 @@
 
 var mongoose = require('mongoose'),
 request = require('request'),
-sendmail = require('sendmail')({
-    logger: {
-      debug: console.log,
-      info: console.info,
-      warn: console.warn,
-      error: console.error
-    },
-    silent: false,
-    devPort: 1025, // Default: False
-    devHost: 'localhost', // Default: localhost
-    smtpPort: 2525, // Default: 25
-    smtpHost: 'localhost' // Default: -1 - extra smtp host after resolveMX
-  }),
+sendmail = require('sendmail'),
 DoWebPaymentResult = mongoose.model('DoWebPaymentResult');
 
 //getwebpament details
